e3972dcb192fce1bb574cbf88629068054cf2987
Brian Dahlem <bdahlem@gmail.com>
2021-03-06 16:13:34 -0700

    reassigned SPI pins

--- ports/atmel-samd/boards/sugarcookie/mpconfigboard.h
+++ ports/atmel-samd/boards/sugarcookie/mpconfigboard.h
@@ -1,14 +1,15 @@
 #define MICROPY_HW_BOARD_NAME "SugarCookie"
 #define MICROPY_HW_MCU_NAME "samd21e18"
 
-//#define SPI_FLASH_BAUDRATE  (8000000)
+// #define SPI_FLASH_BAUDRATE  (8000000)
 
 #define MICROPY_HW_LED_STATUS   (&pin_PA27)
 
-#define SPI_FLASH_MOSI_PIN          (&pin_PA19)
-#define SPI_FLASH_MISO_PIN          (&pin_PA18)
+
 #define SPI_FLASH_SCK_PIN           (&pin_PA17)
-#define SPI_FLASH_CS_PIN            (&pin_PA16)
+#define SPI_FLASH_MOSI_PIN          (&pin_PA16)
+#define SPI_FLASH_MISO_PIN          (&pin_PA19)
+#define SPI_FLASH_CS_PIN            (&pin_PA18)
 
 // These are pins not to reset.
 #define MICROPY_PORT_A        ( 0 )
@@ -21,10 +22,17 @@
 #define DEFAULT_I2C_BUS_SCL (&pin_PA23)
 #define DEFAULT_I2C_BUS_SDA (&pin_PA22)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_PA06)
-#define DEFAULT_SPI_BUS_MOSI (&pin_PA04)
-#define DEFAULT_SPI_BUS_MISO (&pin_PA05)
 
+#define DEFAULT_SPI_BUS_SCK (&pin_PA07)
+#define DEFAULT_SPI_BUS_MOSI (&pin_PA06)
+#define DEFAULT_SPI_BUS_MISO (&pin_PA05)
+#define DEFAULT_SPI_BUS_CS (&pin_PA04)
+
+/*
+#define DEFAULT_SPI_BUS_SCK (&pin_PA17)
+#define DEFAULT_SPI_BUS_MOSI (&pin_PA16)
+#define DEFAULT_SPI_BUS_MISO (&pin_PA19)
+*/
 #define DEFAULT_UART_BUS_RX (&pin_PA14)
 #define DEFAULT_UART_BUS_TX (&pin_PA15)
 
